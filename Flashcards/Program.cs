2d2157cd1e22441c5ba4b2da23a8d2235556c1ed
Pablo <pablo.aguirredesouza@ebgames.com>
2021-11-03 14:57:27 +1000

    Connected to SQL Local Db with Create DB command

--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Data.SqlClient;
 
 namespace Flashcards
 {
@@ -6,7 +7,37 @@ namespace Flashcards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QueryDb(); 
+        }
+
+        static internal void QueryDb()
+        {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;";
+            try
+            {
+
+                SqlConnection conn = new SqlConnection(connectionString);
+                using (conn)
+                {
+                    conn.Open();
+                    var tableCmd = conn.CreateCommand();
+                    tableCmd.CommandText =
+                        $@"IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'MyTestDataBase')
+                           BEGIN
+                             CREATE DATABASE MyTestDataBase;
+                           END;
+                         ";
+                    tableCmd.ExecuteNonQuery();
+                    conn.Close();
+
+                    Console.WriteLine("Table Created");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
         }
     }
 }
